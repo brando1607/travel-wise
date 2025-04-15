3c16cf669ae38ab2531e8ce84dc130091b21da7e
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-04-15 12:18:19 -0400

    chore(gateway): filter and interceptor implemented

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -2,18 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { port } from './utils/port.utils';
-import { CustomFilter } from './utils/custom.filter';
+import { CustomInterceptor } from './utils/custom.interceptor';
+import { CustomExceptionFilter } from './utils/custom.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
+  const microservice = app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: { host: 'localhost', port: port },
   });
 
-  await app.startAllMicroservices();
+  app.useGlobalFilters(new CustomExceptionFilter());
+  app.useGlobalInterceptors(new CustomInterceptor());
 
-  app.useGlobalFilters(new CustomFilter());
+  microservice.useGlobalFilters(new CustomExceptionFilter());
+  microservice.useGlobalInterceptors(new CustomInterceptor());
+
+  await app.startAllMicroservices();
 
   await app.listen(port ?? 3000, () => {
     console.log(`Api gateway listening on port http://localhost:${port}`);
