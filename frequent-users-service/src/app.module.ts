6fd45cd0714e83c660062b5a835cd35ea86a8faf
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-04-15 12:26:25 -0400

    chore(frequent-users): module configured

--- frequent-users-service/src/app.module.ts
+++ frequent-users-service/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PrismaModule } from './prisma/prisma.module';
+import { FrequentUsersController } from './frequent-users/frequent.users.controller';
+import { FrequentUsersService } from './frequent-users/frequent.users.service';
 
 @Module({
   imports: [PrismaModule, ClientsModule.register([])],
-  controllers: [],
-  providers: [],
+  controllers: [FrequentUsersController],
+  providers: [FrequentUsersService],
 })
 export class AppModule {}
