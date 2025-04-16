6b4da1ba8fea51598dad9f46aff2da705ead1413
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-04-15 20:34:17 -0400

    chore(api-gateway): methods added to controller and service

--- api-gateway/src/frequent-users/frequent.users.controller.ts
+++ api-gateway/src/frequent-users/frequent.users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Post,
+  Get,
+  Delete,
+  Patch,
+  Param,
+  Body,
+  Controller,
+} from '@nestjs/common';
+import { FrequentUsersService } from './frequent.users.service';
+import { PersonalizedResponse } from './types';
 
-@Controller('frequent.users')
-export class FrequentUsersController {}
+@Controller('frequent-users')
+export class FrequentUsersController {
+  constructor(private readonly frequentUsersService: FrequentUsersService) {}
+
+  @Get()
+  async getAllUsers(): Promise<PersonalizedResponse | void> {
+    try {
+      const result = await this.frequentUsersService.getAllUsers();
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get(':memberNumber')
+  async getUser(
+    @Param('memberNumber') memberNumber: number,
+  ): Promise<PersonalizedResponse | void> {
+    try {
+      const result = await this.frequentUsersService.getUser(
+        Number(memberNumber),
+      );
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
--- api-gateway/src/frequent-users/frequent.users.service.ts
+++ api-gateway/src/frequent-users/frequent.users.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { UpdatedUser, PersonalizedResponse } from './types';
 
 @Injectable()
-export class FrequentUsersService {}
+export class FrequentUsersService {
+  constructor(@Inject('FREQUENT-USERS-SERVICE') private client: ClientProxy) {}
+
+  async getAllUsers(): Promise<PersonalizedResponse | void> {
+    try {
+      const result = this.client.send({ cmd: 'getAllUsers' }, {});
+
+      const value = await lastValueFrom(result);
+
+      return value;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getUser(memberNumber: number): Promise<PersonalizedResponse | void> {
+    try {
+      const result = this.client.send({ cmd: 'getUser' }, memberNumber);
+
+      const value = await lastValueFrom(result);
+
+      return value;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateUser({
+    memberNumber,
+    newData,
+  }: {
+    memberNumber: number;
+    newData: UpdatedUser;
+  }): Promise<PersonalizedResponse | void> {
+    try {
+      const result = this.client.send(
+        { cmd: 'updateUser' },
+        { memberNumber, newData },
+      );
+
+      const value = await lastValueFrom(result);
+
+      return value;
+    } catch (error) {
+      return error;
+    }
+  }
+}
