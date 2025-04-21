f188adda0c441a7b84c447b00ab4b8d61987a920
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-04-20 21:48:04 -0400

    chore(api-gateway): register method created

--- api-gateway/src/auth/auth.service.ts
+++ api-gateway/src/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { NewUser, UserData } from './types';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject('FREQUENT-USERS-SERVICE') private UserClient: ClientProxy,
+    @Inject('AUTH-SERVICE') private AuthClient: ClientProxy,
+  ) {}
+
+  async register(userData: UserData): Promise<NewUser> {
+    try {
+      const { password, ...userWithoutPassword } = userData;
+      const createUser = await lastValueFrom(
+        this.UserClient.send({ cmd: 'createUser' }, userWithoutPassword),
+      );
+
+      const memberNumber = createUser.data.memberNumber;
+
+      //add password to db
+
+      await lastValueFrom(
+        this.AuthClient.send(
+          { cmd: 'addPassword' },
+          { memberNumber, password },
+        ),
+      );
+
+      return createUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
