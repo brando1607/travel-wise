7dc4d670cf50e356c120bc8ed46788a3875b202e
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-04-20 21:53:54 -0400

    chore(api-gateway): register method created

--- api-gateway/src/auth/auth.controller.ts
+++ api-gateway/src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Post,
+  Get,
+  Delete,
+  Patch,
+  Param,
+  Body,
+  Controller,
+} from '@nestjs/common';
+import { NewUser, UserData } from './types';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async register(@Body() userData: UserData): Promise<NewUser> {
+    try {
+      const result = await this.authService.register(userData);
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
