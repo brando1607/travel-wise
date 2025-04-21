6f38dc6356cb7f2a1c84a2b92e5675a2cfc048f8
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-04-20 21:56:28 -0400

    chore(auth): types updated

--- auth-service/src/auth/types.ts
+++ auth-service/src/auth/types.ts
@@ -18,7 +18,7 @@ type Users = {
 
 export type UpdatedUser = Partial<Users>;
 
-export type NewUser = Omit<Users, 'memberNumber' | 'email'>;
+export type NewUser = Omit<Users, 'memberNumber'>;
 
 export type PersonalizedResponse = {
   message: string;
