8a43dafb98cf76855dda42815c415912460bcc93
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-04-20 21:45:30 -0400

    chore(api-gateway): types created

---
+++ api-gateway/src/auth/types.ts
@@ -0,0 +1,37 @@
+enum Status {
+  ACTIVE,
+  BLOCKED,
+  INACTIVE,
+  SUSPENDED,
+}
+
+type Users = {
+  memberNumber: number;
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  dateOfBirth: Date;
+  country: string;
+  status: Status;
+  bookings: string[];
+};
+
+export type UpdatedUser = Partial<Users>;
+
+export type NewUser = Omit<Users, 'memberNumber' | 'password'>;
+
+export type UserData = Omit<Users, 'memberNumber'>;
+
+export type PersonalizedResponse = {
+  message: string;
+  statusCode: number;
+  data?: any;
+};
+
+export type Login = string | number;
+
+export type Response = {
+  result: boolean;
+  message?: string;
+};
