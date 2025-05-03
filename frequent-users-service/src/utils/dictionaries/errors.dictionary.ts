8435e959baa7261b8a68eeda08d03803cf818b9a
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-05-03 17:37:04 -0400

    chore(frequent-users): new errors added

--- frequent-users-service/src/utils/dictionaries/errors.dictionary.ts
+++ frequent-users-service/src/utils/dictionaries/errors.dictionary.ts
@@ -3,9 +3,26 @@ export const errors = {
     user: { message: 'User not found', statusCode: 404 },
     users: { message: 'No users yet', statusCode: 404 },
     email: { message: 'Email not found', statusCode: 404 },
+    changeRequest: {
+      message: 'Name change request not found',
+      statusCode: 404,
+    },
   },
   conflict: {
     user: { message: 'User already exists', statusCode: 409 },
     email: { message: 'Email already in use', statusCode: 409 },
+  },
+  missing: {
+    entry: { message: 'Missing or empty entry', statusCode: 400 },
+    nameNotDifferent: {
+      message: 'Name entered is the same as the current one',
+      statusCode: 400,
+    },
+  },
+  forbidden: {
+    nameChangeNotAllowed: {
+      message: 'Name change not authorized',
+      statusCode: 403,
+    },
   },
 };
