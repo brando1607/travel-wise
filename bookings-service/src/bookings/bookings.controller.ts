282f36dd6dfa9d6db141a3f8bb472d8a9d9d9621
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-05-23 23:13:34 -0400

    chore(bookings): controller configured

--- bookings-service/src/bookings/bookings.controller.ts
+++ bookings-service/src/bookings/bookings.controller.ts
@@ -1,4 +1,28 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { BookingsService } from './bookings.service';
 
 @Controller('bookings')
-export class BookingsController {}
+export class BookingsController {
+  constructor(private bookingsService: BookingsService) {}
+
+  @MessagePattern({ cmd: 'getAvailabilityWithAirportCode' })
+  async getAvailabilityWithAirportCode({
+    origin,
+    destination,
+  }: {
+    origin: string;
+    destination: string;
+  }): Promise<number | void> {
+    try {
+      const result = await this.bookingsService.getAvailabilityWithAirportCode({
+        origin,
+        destination,
+      });
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
