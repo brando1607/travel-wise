0c49f0e9f5846b86089a612354efb9de4f4da782
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-05-23 23:13:02 -0400

    chore(bookings): http module imported

--- bookings-service/src/bookings/bookings.module.ts
+++ bookings-service/src/bookings/bookings.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { BookingsController } from './bookings.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   providers: [BookingsService],
   controllers: [BookingsController],
-  imports: [PrismaModule],
+  imports: [PrismaModule, HttpModule],
 })
 export class BookingsModule {}
