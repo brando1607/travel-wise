4bb8e9eccb128e57dd305fe7c293e0c1379a1576
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-06-20 21:56:19 -0400

    performance: email service dockerized

--- email-service/src/main.ts
+++ email-service/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://localhost'],
+      urls: ['amqp://rabbitmq:5672'],
       queue: 'email_queue',
       queueOptions: { durable: true },
     },
