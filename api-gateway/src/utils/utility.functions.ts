51966d85b4378bc5493e6fcb5f75e8f6e8072fa2
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-07-03 22:38:03 -0400

    feat: function added

--- api-gateway/src/utils/utility.functions.ts
+++ api-gateway/src/utils/utility.functions.ts
@@ -1,61 +1,66 @@
 import { HttpException } from '@nestjs/common';
 import { validateCabin, validateDate } from 'src/bookings/schemas/functions';
 import { DateTime } from 'luxon';
-import { Search } from 'src/bookings/types';
-import { DataValidation } from 'src/bookings/types';
+import { DataValidation, Itinerary } from 'src/bookings/types';
 
-export function validateDateFormatAndCabin(data: Search): DataValidation {
+export function validateDateFormatAndCabin(data: Itinerary): DataValidation {
   let fare: number = 0;
-  let date: string = '';
-  let origin: string = '';
-  let destination: string = '';
-  let cabin: string = '';
+  const cabin = data.cabin.toLowerCase();
+  const date = data.date;
+  const origin = data.origin.toLowerCase();
+  const destination = data.destination.toLowerCase();
+  const dateFormat = validateDate({ date });
+  const validDate = DateTime.fromFormat(date, 'dd-MM-yyyy');
+  const futureDate = DateTime.now().plus({ days: 350 }).startOf('day');
+  const today = DateTime.now().startOf('day');
+  const checkCabin = validateCabin({ cabin });
 
-  for (let i = 0; i < data.flights.length; i++) {
-    cabin = data.flights[i].cabin.toLowerCase();
-    date = data.flights[i].date;
-    origin = data.flights[i].origin.toLowerCase();
-    destination = data.flights[i].destination.toLowerCase();
-    const dateFormat = validateDate({ date });
-    const validDate = DateTime.fromFormat(date, 'dd-MM-yyyy');
-    const futureDate = DateTime.now().plus({ days: 350 }).startOf('day');
-    const today = DateTime.now().startOf('day');
-    const checkCabin = validateCabin({ cabin });
+  if (!dateFormat.success) {
+    throw new HttpException(dateFormat.error.errors[0].message, 400);
+  }
 
-    if (!dateFormat.success) {
-      throw new HttpException(dateFormat.error.errors[0].message, 400);
-    }
+  if (!validDate.isValid) {
+    throw new HttpException(`${date} is not a valid date.`, 400);
+  }
 
-    if (!validDate.isValid) {
-      throw new HttpException(`${date} is not a valid date.`, 400);
-    }
+  if (validDate < today) {
+    throw new HttpException(`Date can't be before today`, 400);
+  }
 
-    if (validDate < today) {
-      throw new HttpException(`Date can't be before today`, 400);
-    }
+  if (validDate > futureDate) {
+    throw new HttpException(
+      `Date can't be more than 350 days in the future.`,
+      400,
+    );
+  }
 
-    if (validDate > futureDate) {
-      throw new HttpException(
-        `Date can't be more than 350 days in the future.`,
-        400,
-      );
-    }
+  if (!checkCabin.success) {
+    throw new HttpException(checkCabin.error.errors[0].message, 400);
+  }
 
-    if (!checkCabin.success) {
-      throw new HttpException(checkCabin.error.errors[0].message, 400);
-    }
-
-    if (cabin === 'economy') {
-      fare = 0.05;
-    } else if (cabin === 'premium') {
-      fare = 0.09;
-    } else {
-      fare = 0.14;
-    }
+  if (cabin === 'economy') {
+    fare = 0.05;
+  } else if (cabin === 'premium') {
+    fare = 0.09;
+  } else {
+    fare = 0.14;
   }
 
   return {
     result: true,
     data: { date: date, origin, destination, fare, cabin },
   };
+}
+
+export function obIsBeforeIB(ob: string, ib: string): boolean {
+  try {
+    const outBound = DateTime.fromFormat(ob, 'dd-MM-yyyy');
+    const inBound = DateTime.fromFormat(ib, 'dd-MM-yyyy');
+
+    const isBefore = outBound < inBound;
+
+    return isBefore;
+  } catch (error) {
+    throw error;
+  }
 }
