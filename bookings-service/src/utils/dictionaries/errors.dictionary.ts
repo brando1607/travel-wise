fc4e3025a1921cf942df3642f4e8db5befcc1b9c
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-07-07 17:12:03 -0400

    style: dictionary changed

--- bookings-service/src/utils/dictionaries/errors.dictionary.ts
+++ bookings-service/src/utils/dictionaries/errors.dictionary.ts
@@ -1,7 +1,7 @@
 export const errors = {
   notFound: {
     availability: {
-      message: 'Availability expired, please search again.',
+      message: 'Availability expired or not found, please search again.',
       statusCode: 404,
     },
     passengerInfo: {
