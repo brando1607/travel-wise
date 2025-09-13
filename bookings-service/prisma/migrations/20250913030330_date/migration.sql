600951f0cb915d395223cd2dcd0336e8957be7e9
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-09-13 00:04:06 -0300

    fix: date column removed as it's added in the origin and destination

---
+++ bookings-service/prisma/migrations/20250913030330_date/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `date` on the `itinerary` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "itinerary" DROP COLUMN "date";
